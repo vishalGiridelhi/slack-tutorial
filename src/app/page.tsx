1b44b0ce67aefa61608cb941428742e20bd5b216
vishalGiridelhi <you@example.com>
2024-09-14 21:58:16 +0530

    sat14 coding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
+import { AuthScreen } from "@/features/auth/components/auth-screen";
 
 export default function Home() {
   return (
-    <div className="text-rose-500 font-bold text-2xl">hello world</div>
+    <AuthScreen/>
   );
 }
